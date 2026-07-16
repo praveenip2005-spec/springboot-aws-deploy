60793187323e37008a735f08f17642f0686de6c3
praveenip2005-spec <praveenip2005@gmail.com>
2026-07-16 11:52:15 +0530

    Update TestController.java

--- main/java/com/example/springbootawsdeploy/TestController.java
+++ main/java/com/example/springbootawsdeploy/TestController.java
@@ -1,15 +1,13 @@
 package com.example.springbootawsdeploy;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo")
 public class TestController {
 
-    @GetMapping("/data")
-    public String getData() {
-        return "First message from AWS ECS";
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot AWS Deploy Success";
     }
 }
